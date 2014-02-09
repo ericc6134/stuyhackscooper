4ecd32649889b439444bbda9b1cb083b8bdc8c3f
ericc6134 <ericc6134@gmail.com>
2014-02-09 07:22:34 -0500

    oh right, github exists

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 stuyhackscooper
 ===============
+
+<b>Utilities Used</b>
+* virtualenv
+* pip
+* pip install flask
+* pip install jinja2
+* pip install google maps
---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+
+import tw
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    tweet = tw.get_tweet()
+    return render_template("home.html", tweet=tweet)
+    
+if __name__ == "__main__":
+    app.run()
+        
