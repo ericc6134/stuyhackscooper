96dccbe61f78bce520cd2ea55a7abbd43d2bba96
ericc6134 <ericc6134@gmail.com>
2014-02-09 11:00:15 -0500

    WORKING with localStorage

--- tw.py
+++ tw.py
@@ -3,7 +3,6 @@ from twython import Twython
 KEYS = open("KEYS", "r");
 APP_KEY = KEYS.readline().strip('\n')
 ACCESS_TOKEN = KEYS.readline().strip('\n') # KEYS is a local file
-
 twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
 
 def get_tweet():
@@ -12,12 +11,10 @@ def get_tweet():
         for i in range(len(statuses)):
             try:
                 tweet = { "user": statuses[i]["user"]["screen_name"],
-                          "created_at": statuses[i]["created_at"],
+                          "created_at": statuses[i]["created_at"][0:,
                           "coordinates": statuses[i]["coordinates"]["coordinates"],
                           "text": statuses[i]["text"]
                         }
                 return tweet
             except:
                 pass
- 
-print get_tweet()
